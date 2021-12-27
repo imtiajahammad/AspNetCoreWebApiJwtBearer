84298b023a5bbab78ab8d2ab14cfd403283d81f5
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2021-12-27 12:13:41 +0600

    AddJwtBearer to startup

--- AspNetCoreWebApiJwtBearer/Startup.cs
+++ AspNetCoreWebApiJwtBearer/Startup.cs
@@ -11,6 +11,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using AspNetCoreWebApiJwtBearer.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+
+
 namespace AspNetCoreWebApiJwtBearer
 {
     public class Startup
@@ -26,6 +32,31 @@ namespace AspNetCoreWebApiJwtBearer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            #region jwt
+            var bindJwtSettings = new JwtSettings();
+            Configuration.Bind("JsonWebTokenKeys", bindJwtSettings);
+            services.AddSingleton(bindJwtSettings);
+            services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options => {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = bindJwtSettings.ValidateIssuerSigningKey,
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(bindJwtSettings.IssuerSigningKey)),
+                    ValidateIssuer = bindJwtSettings.ValidateIssuer,
+                    ValidIssuer = bindJwtSettings.ValidIssuer,
+                    ValidateAudience = bindJwtSettings.ValidateAudience,
+                    ValidAudience = bindJwtSettings.ValidAudience,
+                    RequireExpirationTime = bindJwtSettings.RequireExpirationTime,
+                    ValidateLifetime = bindJwtSettings.RequireExpirationTime,
+                    ClockSkew = TimeSpan.FromDays(1),
+                };
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +70,10 @@ namespace AspNetCoreWebApiJwtBearer
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+
+            app.UseAuthentication();
+
 
             app.UseAuthorization();
 
