b5d40dec75c10c0ef80336f7914485678e5add98
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2021-12-27 22:36:20 +0600

    user models

---
+++ AspNetCoreWebApiJwtBearer/Models/Users.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWebApiJwtBearer.Models
+{
+    public class Users
+    {
+        public string UserName
+        {
+            get;
+            set;
+        }
+        public Guid Id
+        {
+            get;
+            set;
+        }
+        public string EmailId
+        {
+            get;
+            set;
+        }
+        public string Password
+        {
+            get;
+            set;
+        }
+    }
+}
